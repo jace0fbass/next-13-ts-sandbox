aacfc3052af699cd9b2e126b2a413ea7aad61b77
Jason <jajones414@gmail.com>
2023-08-06 10:45:43 -0500

    Corrected 'use client' error in files. Using useRouter for button navigation in search page

--- app/notes/search/page.tsx
+++ app/notes/search/page.tsx
@@ -1,12 +1,16 @@
-"use-client";
+"use client";
 
 import styles from '../../page.module.css'
+import { useRouter } from 'next/navigation'
 
 export default function SearchNote() {
+  const router = useRouter();
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <h1>Search Note Page</h1>
+        <button type="button" onClick={() => router.push('/notes')}>Notes</button> 
       </div>
     </main>
   )
