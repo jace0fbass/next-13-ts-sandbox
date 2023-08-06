15e76f9596bce2f3b7d9c2f11eddfddac188b5d9
Jason <jajones414@gmail.com>
2023-08-06 08:16:14 -0500

    Removed starter code and added static routes

---
+++ app/dashboard/analytics/page.tsx
@@ -0,0 +1,13 @@
+"use-client";
+
+import styles from '../../page.module.css'
+
+export default function Analytics() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <h1>Analytics Page</h1>
+      </div>
+    </main>
+  )
+}
---
+++ app/dashboard/comments/page.tsx
@@ -0,0 +1,13 @@
+"use-client";
+
+import styles from '../../page.module.css'
+
+export default function Comments() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <h1>Comments Page</h1>
+      </div>
+    </main>
+  )
+}
