b05f3c349f45cd0e51a45e749da0e2d352fbe78f
Jason <jajones414@gmail.com>
2023-08-06 08:23:05 -0500

    Added dynamic route

---
+++ app/notes/[...note]/page.tsx
@@ -0,0 +1,13 @@
+"use-client";
+
+import styles from '../../page.module.css'
+
+export default function Note() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <h1>Note Page</h1>
+      </div>
+    </main>
+  )
+}
---
+++ app/notes/page.tsx
@@ -0,0 +1,13 @@
+"use-client";
+
+import styles from '../page.module.css'
+
+export default function Notes() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <h1>Notes Page</h1>
+      </div>
+    </main>
+  )
+}
